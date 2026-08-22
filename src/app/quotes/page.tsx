93bb4e87cdbb08cf8a11b3daa2bfba7507334394
Nick <nick.heyn@gmail.com>
2026-08-21 19:14:08 -0500

    Updating metadata

--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -13,18 +13,22 @@ const cormorant = Cormorant_Garamond({
 export default function QuotesPage() {
   const [quote, setQuote] = useState<(typeof quoteList)[0] | null | undefined>();
 
-  const getRandomQuote = () => {
+  const setRandomQuote = () => {
     const randomIndex = Math.floor(Math.random() * quoteList.length);
-    return quoteList[randomIndex];
+    const newQuote = quoteList[randomIndex];
+    setQuote(newQuote);
   };
 
   useEffect(() => {
     // eslint-disable-next-line react-hooks/set-state-in-effect
-    setQuote(getRandomQuote());
+    setRandomQuote();
   }, []);
 
   return (
-    <main className={`${cormorant.className} flex flex-col items-center justify-center h-screen`}>
+    <main
+      className={`${cormorant.className} flex flex-col items-center justify-center h-screen text-center`}
+      onClick={setRandomQuote}
+    >
       {!!quote && (
         <>
           <span className="text-3xl italic">&quot;{quote?.quote}&quot;</span>
